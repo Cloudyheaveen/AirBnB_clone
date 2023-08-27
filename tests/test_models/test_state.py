f75d79429cb86fe0d24661bcda8ea7d2f17770d9
anas-hfd <anas1.hfd@gmail.com>
2023-08-27 07:20:24 -0700

    Airbnb clone

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""State Unittest"""
+
+import unittest
+from datetime import datetime
+import time
+from models.state import State
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+
+    """Test Cases"""
+
+    def setUp(self):
+        """Setup"""
+        pass
+
+    def tearDown(self):
+        """Teardown"""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """FileStorage data reset"""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests the instantiation"""
+
+        b = State()
+        self.assertEqual(str(type(b)), "<class 'models.state.State'>")
+        self.assertIsInstance(b, State)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes"""
+        attributes = storage.attributes()["State"]
+        o = State()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+if __name__ == "__main__":
+    unittest.main()
